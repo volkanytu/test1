9a3db5d04802ef2761ddde16eeba97cf431ff3ee
Volkan SERTER <volkan.serter@sahibinden.com>
2015-12-12 15:10:06 +0200

    first

---
+++ UpdateStatusTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace UpdateStatusTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            inboundserv.InboundService ser = new inboundserv.InboundService();
+            ser.Timeout = 5000;
+
+            while (true)
+            {
+                Stopwatch sWatch = new Stopwatch();
+                sWatch.Start();
+
+                Console.WriteLine("Ready_START|" + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+
+                try
+                {
+                    ser.UpdateCrmStatus("123", "Ready");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("EX_READY:" + ex.Message + " " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+                }
+
+                sWatch.Stop();
+                Console.WriteLine("Ready_END  |" + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss") + " | " + sWatch.Elapsed.TotalMilliseconds.ToString());
+
+                Thread.Sleep(1000);
+
+                sWatch.Restart();
+
+                Console.WriteLine("--------------------------------------------");
+
+                Console.WriteLine("NotReady_START|" + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+
+                try
+                {
+                    ser.UpdateCrmStatus("123", "NotReady");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("EX_NOT_READY:" + ex.Message + " " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+                }
+
+                sWatch.Stop();
+                Console.WriteLine("NotReady_END  |" + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss") + " | " + sWatch.Elapsed.TotalMilliseconds.ToString());
+            }
+        }
+    }
+}
